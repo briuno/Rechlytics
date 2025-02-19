80a82d1b273b4bf120a2759bed7e0ac6485c6dab
Bruno Reche <49537489+briuno@users.noreply.github.com>
2025-02-19 11:50:44 -0300

    Teste do banco de dados V.4

--- db_test.php
+++ db_test.php
@@ -3,11 +3,11 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// Configuração do banco de dados
-$servername = "127.0.0.1:3306"; // Verifique se Hostinger indicou um servidor diferente
-$username = "u332555040_rechlytics_use"; // Seu usuário MySQL
-$password = "B1?r~s?"; // Sua senha real
-$dbname = "u332555040_rechlytics_db"; // Nome do banco de dados
+// Credenciais corrigidas
+$servername = "193.203.175.215"; // Servidor correto do banco de dados
+$username = "u332555040_rechlytics_use"; // Usuário correto
+$password = "B1?r~s?"; // Senha correta
+$dbname = "u332555040_rechlytics_db"; // Nome correto do banco de dados
 
 // Criar conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -16,6 +16,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Erro na conexão: " . $conn->connect_error);
 }
-echo "Conectado com sucesso!!";
+echo "Conectado com sucesso!";
 ?>
-
