96ef659a480df368187b4b3c3944842a2689315b
Bruno Reche <49537489+briuno@users.noreply.github.com>
2025-02-19 11:58:38 -0300

    Alteração de senha do banco

--- db_test.php
+++ db_test.php
@@ -6,7 +6,7 @@ ini_set('display_errors', 1);
 // Credenciais corrigidas
 $servername = "193.203.175.215"; // Servidor correto do banco de dados
 $username = "u332555040_rechlytics_use"; // Usuário correto
-$password = "B1?r~s?"; // Senha correta
+$password = "Br220809#"; // Senha correta
 $dbname = "u332555040_rechlytics_db"; // Nome correto do banco de dados
 
 // Criar conexão
