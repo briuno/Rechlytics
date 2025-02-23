dd8e7c87aa1d2fb4bf974428a1d051907cc51776
Bruno Reche <brunorechegil@gmail.com>
2025-02-23 00:19:54 -0300

    teste

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rechlytics - Página Inicial</title>
+    <title>Rechlytics - Página Inicial teste</title>
 </head>
 <body>
     <h2>Bem-vindo ao Rechlytics</h2>
     <p>Sua plataforma para análise e visualização de dados.</p>
-    
-    <a href="login.php"><button>Entrar</button></a>
-    <a href="cadastro.php"><button>Cadastrar</button></a>
+
+    <a href="/auth/login.php"><button>Entrar</button></a>
+    <a href="/auth/cadastro.php"><button>Cadastrar</button></a>
 </body>
 </html>
