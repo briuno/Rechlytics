32305cb1c4bc1e3ac27a5fc94fbb62091b3160dc
Bruno Reche <49537489+briuno@users.noreply.github.com>
2025-06-04 10:36:20 -0300

    Ajuste emails

--- controllers/email.php
+++ controllers/email.php
@@ -13,9 +13,9 @@ function enviarEmail($para, $assunto, $mensagem) {
         $mail->Host = 'smtp.hostinger.com'; // Se usar Gmail, troque por smtp.gmail.com
         $mail->SMTPAuth = true;
         $mail->Username = 'suporte@rechlytics.com'; // Seu e-mail SMTP
-        $mail->Password = 'Rechlytics2025#'; // Senha do e-mail
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // TLS
-        $mail->Port = 587; // Porta padrão do SMTP
+        $mail->Password = 'Bruno007#@'; // Senha do e-mail
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // SMTPS
+        $mail->Port = 465; // Porta padrão do SMTP
 
         // Configuração do remetente e destinatário
         $mail->setFrom('suporte@rechlytics.com', 'Rechlytics Suporte');
